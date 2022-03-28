41f8e0f079e0a6489cc688771c2c60b8daaa514b
♿ <biznes@2.pl>
2022-03-28 23:00:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ global.db = {
     filter: new Database({ path: "./db/filter.json", sync: false, space: 4 })
 };
 
-if(!db.routes.size || !db.trips.size || !db.stops.size || !db.shapes.size) load();
+if (!db.routes.size || !db.trips.size || !db.stops.size || !db.shapes.size) load();
 cron.schedule('0 3 * * *', load);
 
-if(!db.vehicles.size || !db.filter.size) loadVehicles();
+//if (!db.vehicles.size || !db.filter.size) loadVehicles();
 cron.schedule('0 4 */3 * *', loadVehicles);
 
-app.get("/trip",  async(req, res) => {
+app.get("/trip", async (req, res) => {
     let trip = db.trips.get(req.query.trip);
     if (!trip) return res.code(404).send("Trip not found");
     let shape = db.shapes.get(trip.shape);
-    res.send({
+    return {
         line: trip.line,
         headsign: trip.headsign,
         shapes: shape,
@@ -37,10 +37,39 @@ app.get("/trip",  async(req, res) => {
                 time: stop.departure
             }
         })
-    });
+    };
+});
+
+app.get("/predict", async () => {
+    let naTrasie = db.trips.filter(trip => (trip.line.includes("R") || (trip.line.includes("S") && trip.trip.includes("DP"))) && (trip.stops[0].arrival < Date.now() && trip.stops[trip.stops.length - 1].departure > Date.now()));
+    return removeDup(naTrasie.map(x => {
+        let shape = db.shapes.get(x.shape);
+        let time = (x.stops[x.stops.length - 1].departure - x.stops[0].arrival) / 1000;
+        let timePerShape = time / shape.length;
+
+        let when = shape.map((s, i) => {
+            return {
+                location: s,
+                time: Math.floor(x.stops[0].arrival + (i * timePerShape * 1000))
+            }
+        });
+
+        let nearest = when.reduce((a, b) => Math.abs(a.time - Date.now()) < Math.abs(b.time - Date.now()) ? a : b);
+        return {
+            line: x.line,
+            trip: x.trip,
+            headsign: x.headsign,
+            location: nearest.location,
+        };
+    }), "location");
 });
 
 app.listen(3000, (err, address) => {
     if (err) throw err;
     console.log(`server listening on ${address}`);
 });
+
+function removeDup(arr, key) {
+    let seen = {};
+    return arr.filter((item) => seen.hasOwnProperty(item[key]) ? false : (seen[item[key]] = true));
+}
