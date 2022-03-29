b3ae6acf4165af4d65dbd000c5a5dbb3b86898e1
♿ <biznes@2.pl>
2022-03-29 20:23:26 +0200

    naprawić R6* do wileniaka i returnować gdy braknie shape'a

--- predict.js
+++ predict.js
@@ -1,38 +1,35 @@
 const { point, nearestPointOnLine, lineString } = require("@turf/turf");
 
-module.exports = async() => {
+module.exports = async () => {
     let naTrasie = db.trips.filter(trip => (trip.line.includes("R") || trip.line.includes("S")) && (trip.stops[0].arrival < Date.now() && trip.stops[trip.stops.length - 1].departure > Date.now()));
-    return naTrasie.map(x => {
+    return removeDup(naTrasie.map(x => {
         let shape = lineString(db.shapes.get(x.shape));
-        return {
-            shape,
-            stops: x.stops.map(stop => {
-                let stopData = db.stops.get(stop.id);
-                return {
-                    ...stop,
-                    onLine: nearestPointOnLine(shape, point(stopData.location), {units: 'meters'})
-                }
-            })
-        }
-        //return x.stops;
-    })[0];
-};
-
-function removeDup(arr, key) {
-    let seen = {};
-    return arr.filter((item) => seen.hasOwnProperty(item[key]) ? false : (seen[item[key]] = true));
-}
-
-/*
-let time = (x.stops[x.stops.length - 1].departure - x.stops[0].arrival) / 1000;
-        let timePerShape = time / shape.length;
-
-        let when = shape.map((s, i) => {
+        let stops = x.stops.map(stop => {
+            let stopData = db.stops.get(stop.id);
             return {
-                location: s,
-                time: Math.floor(x.stops[0].arrival + (i * timePerShape * 1000))
+                ...stop,
+                ...stopData,
+                index: nearestPointOnLine(shape, point(stopData.location), { units: 'meters' }).properties.index
             }
         });
+        
+        let nextStop = stops.filter(stop => stop.arrival > Date.now()).shift();
+        let beforeStop = stops.filter(stop => stop.departure <= Date.now()).pop();
+
+        let shapes = shape.geometry.coordinates.slice(beforeStop.index, nextStop.index);
+        let timeForShape = (nextStop.arrival - beforeStop.departure) / shapes.length;
+
+        let when = shapes.map((s, i) => {
+            return {
+                location: s,
+                time: Math.floor(stops[0].arrival + i * timeForShape)
+            }
+        });
+
+        if(!when.length) {
+            console.log(`Rozjebany kurs: ${x.line} > ${x.headsign} (${x.trip})`);
+            return null;
+        }
 
         let nearest = when.reduce((a, b) => Math.abs(a.time - Date.now()) < Math.abs(b.time - Date.now()) ? a : b);
         return {
@@ -41,4 +38,10 @@ let time = (x.stops[x.stops.length - 1].departure - x.stops[0].arrival) / 1000;
             headsign: x.headsign,
             location: nearest.location,
         };
-*/
+    }).filter(x => x), "location");
+};
+
+function removeDup(arr, key) {
+    let seen = {};
+    return arr.filter((item) => seen.hasOwnProperty(item[key]) ? false : (seen[item[key]] = true));
+}
